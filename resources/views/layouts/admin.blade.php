de71b9567189f11ed20ad8ea663747951ffb9f51
elinaldoA <83353178+elinaldoA@users.noreply.github.com>
2024-04-03 17:42:25 -0300

    Site

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -150,7 +150,7 @@
         <footer class="sticky-footer bg-white">
             <div class="container my-auto">
                 <div class="copyright text-center my-auto">
-                    <span>Copyright &copy; Elinaldo Agostinho 2024</span>
+                    <span>Copyright &copy;<script>document.write(new Date().getFullYear());</script>. Todos os direitos reservados</span>
                 </div>
             </div>
         </footer>
