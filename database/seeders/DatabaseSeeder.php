15d31fb753a94a2616f2a4cffdd91ad7f95ff830
elinaldoA <83353178+elinaldoA@users.noreply.github.com>
2024-04-10 16:16:29 -0300

    correcoes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,6 @@ class DatabaseSeeder extends Seeder
             SituacaoContaSeeder::class,
             CategoriaSeeder::class,
             UserSeeder::class,
-            //ContaSeeder::class,
         ]);
     }
 }
